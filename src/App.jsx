c57e0165f3e612e1fbe795a7bddad07d5f704ada
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-25 22:39:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
 
       {/* Hero */}
       <Hero />
+
+      {/* About */}
+      <About />
     </div>
   );
 }
