b02cf2ba177f05d8a363ffebd2f1e38431b3624d
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-25 23:34:13 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
 
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+    </main>
+  );
+};
+
+export default Home;
