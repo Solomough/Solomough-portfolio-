1e0daa143af8eafd8731bdc9d8faac34af68bfd4
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 13:23:01 +0100

    Update BlogPage.jsx

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -3,8 +3,11 @@ import Blog from "../components/Blog";
 
 function BlogPage() {
   return (
-    <div className="pt-20">
-      <Blog />
+    <div className="py-20 px-6 bg-gray-900 text-gray-100 min-h-screen">
+      <div className="max-w-6xl mx-auto">
+        {/* Blog Component */}
+        <Blog />
+      </div>
     </div>
   );
 }
