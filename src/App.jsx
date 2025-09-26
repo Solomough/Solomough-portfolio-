d0d5cf3c44b4fe6c95adfc0d09b4c2da2766ffb0
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 13:21:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FloatingWhatsApp from "./components/FloatingWhatsApp";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-
 import Home from "./pages/Home";
 import BlogPage from "./pages/BlogPage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
-      {/* Navbar */}
-      <Navbar />
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
+        {/* Navbar */}
+        <Navbar />
 
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<BlogPage />} />
-      </Routes>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<BlogPage />} />
+        </Routes>
 
-      {/* Footer */}
-      <Footer />
+        {/* Footer */}
+        <Footer />
 
-      {/* Floating WhatsApp */}
-      <FloatingWhatsApp />
-
-      {/* Scroll To Top Button */}
-      <ScrollToTopButton />
-    </div>
+        {/* Floating WhatsApp */}
+        <FloatingWhatsApp />
+      </div>
+    </Router>
   );
 }
 
