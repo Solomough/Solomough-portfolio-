1e3fe8036eaa7ce3657d8d9a08b3b7faac50e5cb
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 12:21:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
+  server: {
+    port: 5173, // default port for vite dev server
+    open: true
   },
-  base: "./"
-});
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+})
