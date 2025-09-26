d59a844fb34a8f3d36e27a55b788b3dc7333d3d1
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 02:03:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ensures assets resolve correctly on Vercel
   build: {
-    outDir: "dist", // default build output
+    outDir: "dist",
   },
+  base: "./"
 });
