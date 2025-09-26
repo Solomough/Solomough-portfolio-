4e43413aab390ccac7317120b96934d771102aad
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 01:40:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import FloatingWhatsApp from "./components/FloatingWhatsApp";
+import Home from "./pages/Home";
+import BlogPage from "./pages/BlogPage";
+import BlogPost from "./pages/BlogPost";
 
-const App = () => {
+function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Blog />
-      <Contact />
-      <Footer />
-      <FloatingWhatsApp />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+        </Routes>
+
+        {/* Footer */}
+        <Footer />
+
+        {/* Floating WhatsApp */}
+        <FloatingWhatsApp />
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
