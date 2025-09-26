de6fdc9391b72544219e406b12ae1d7d229be628
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 13:06:30 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Portfolio from "../components/Portfolio";
+import Projects from "../components/Projects";
+import Blog from "../components/Blog";
 import Contact from "../components/Contact";
 
-const Home = () => {
+function Home() {
   return (
-    <main>
+    <div>
+      {/* Hero Section */}
       <Hero />
+
+      {/* About Section */}
       <About />
-      <Portfolio />
+
+      {/* Projects Section */}
+      <Projects />
+
+      {/* Blog Preview Section */}
+      <Blog />
+
+      {/* Contact Section */}
       <Contact />
-    </main>
+    </div>
   );
-};
+}
 
 export default Home;
