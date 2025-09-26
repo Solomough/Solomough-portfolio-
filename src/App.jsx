9f0e907250632e8b293c2b946316f4c6e325e536
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-26 13:05:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 import Home from "./pages/Home";
 import BlogPage from "./pages/BlogPage";
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
+        {/* Navbar */}
         <Navbar />
+
+        {/* Page Routing */}
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blog" element={<BlogPage />} />
           </Routes>
         </div>
+
+        {/* Footer */}
         <Footer />
+
+        {/* Floating WhatsApp */}
+        <FloatingWhatsApp />
       </div>
     </Router>
   );
